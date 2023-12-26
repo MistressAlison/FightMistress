0419516a9258947994c5a7f5410caec469b02fc7
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-12-26 12:20:41 -0400

    BeatDown damage increased 6 -> 10(13). No longer upgrades to 1 cost.

--- src/main/java/FightMistress/cards/BeatDown.java
+++ src/main/java/FightMistress/cards/BeatDown.java
@@ -3,9 +3,7 @@ package FightMistress.cards;
 import FightMistress.cardmods.GrabMod;
 import FightMistress.cards.abstracts.AbstractEasyCard;
 import basemod.helpers.CardModifierManager;
-import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.red.Anger;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -14,11 +12,10 @@ import static FightMistress.MainModfile.makeID;
 
 public class BeatDown extends AbstractEasyCard {
     public final static String ID = makeID(BeatDown.class.getSimpleName());
-    private static final Color glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
 
     public BeatDown() {
         super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 6;
+        baseDamage = damage = 10;
         CardModifierManager.addModifier(this, new GrabMod(true));
     }
 
@@ -29,8 +26,8 @@ public class BeatDown extends AbstractEasyCard {
 
     @Override
     public void upp() {
-        //upgradeDamage(3);
-        upgradeBaseCost(1);
+        upgradeDamage(3);
+        //upgradeBaseCost(1);
     }
 
     @Override
