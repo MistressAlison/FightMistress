504d4d6ae756c3c08c037174e7480fb8e856a46b
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-12-28 11:02:33 -0400

    Puncture rework

--- src/main/java/FightMistress/cards/Puncture.java
+++ src/main/java/FightMistress/cards/Puncture.java
@@ -1,11 +1,12 @@
 package FightMistress.cards;
 
 import FightMistress.cards.abstracts.AbstractEasyCard;
-import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.PersistFields;
+import FightMistress.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.green.Strike_Green;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static FightMistress.MainModfile.makeID;
 
@@ -14,19 +15,22 @@ public class Puncture extends AbstractEasyCard {
 
     public Puncture() {
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        PersistFields.setBaseValue(this, 2);
+        //PersistFields.setBaseValue(this, 2);
         baseDamage = damage = 4;
+        baseMagicNumber = magicNumber = 1;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
         dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(2);
+        upgradeDamage(1);
+        upgradeMagicNumber(1);
     }
 
     @Override
