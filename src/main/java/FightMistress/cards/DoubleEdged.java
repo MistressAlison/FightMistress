5f5094d8ea389aed6a4c8b9b8c1a5035f605f3ec
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-12-27 23:37:51 -0400

    DoubleEdged now previews the card it will copy

--- src/main/java/FightMistress/cards/DoubleEdged.java
+++ src/main/java/FightMistress/cards/DoubleEdged.java
@@ -3,12 +3,16 @@ package FightMistress.cards;
 import FightMistress.actions.DoAction;
 import FightMistress.cards.abstracts.AbstractEasyCard;
 import FightMistress.ui.SheathedCardManager;
+import FightMistress.util.FormatHelper;
 import FightMistress.util.Wiz;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.red.DualWield;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +20,7 @@ import static FightMistress.MainModfile.makeID;
 
 public class DoubleEdged extends AbstractEasyCard {
     public final static String ID = makeID(DoubleEdged.class.getSimpleName());
+    private AbstractCard lastCard;
 
     public DoubleEdged() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
@@ -31,6 +36,31 @@ public class DoubleEdged extends AbstractEasyCard {
                 SheathedCardManager.addCard(lastAttack.makeStatEquivalentCopy());
             }
         }));
+        rawDescription = cardStrings.DESCRIPTION;
+        lastCard = null;
+        initializeDescription();
+    }
+
+    public void triggerOnGlowCheck() {
+        if (AbstractDungeon.actionManager.cardsPlayedThisCombat.stream().noneMatch(c -> c.type == CardType.ATTACK)) {
+            this.glowColor = Settings.RED_TEXT_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
+    }
+
+    public void applyPowers() {
+        super.applyPowers();
+        if (AbstractDungeon.actionManager.cardsPlayedThisCombat.stream().anyMatch(c -> c.type == CardType.ATTACK)) {
+            ArrayList<AbstractCard> attacks = AbstractDungeon.actionManager.cardsPlayedThisCombat.stream().filter(c -> c.type == CardType.ATTACK).collect(Collectors.toCollection(ArrayList::new));
+            AbstractCard preview = attacks.get(attacks.size()-1);
+            if (preview != lastCard) {
+                lastCard = preview;
+                cardsToPreview = lastCard.makeStatEquivalentCopy();
+                this.rawDescription = cardStrings.DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0] + FormatHelper.prefixWords(lastCard.name, "[#efc851]", "[]") + cardStrings.EXTENDED_DESCRIPTION[1];
+                this.initializeDescription();
+            }
+        }
     }
 
     @Override
